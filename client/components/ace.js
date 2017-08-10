b397cf43c1e126808132e22c4207ac46503b2a55
Alice Chuang <bomberetta@gmail.com>
2017-08-10 16:09:47 -0400

    setState

--- client/components/ace.js
+++ client/components/ace.js
@@ -12,26 +12,59 @@ import 'brace/theme/github';
 export default class Ace extends Component {
     constructor(){
         super();
+        this.state={
+            aceEditorValue:''
+        };
         this.onChange=this.onChange.bind(this);
+        this.onClick=this.onClick.bind(this);
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.aceEditorValue !== nextState.aceEditorValue) {
+            return false
+        } else {
+            return true;
+        }
+    }
 
     onChange(newValue) {
-        console.log('change', newValue);
+        console.log('change', newValue, this.state);
+        this.setState({
+            aceEditorValue: newValue
+        });
     }
+
+    onClick(){
+        console.log('test',eval(this.state.aceEditorValue));
+    }
+
+
 
 // Render editor
     render(){
         return(
             <div>
-            <h1>HELOO</h1>
+            <h1>HELLO</h1>
+
             <AceEditor
                 mode="javascript"
                 theme="github"
                 onChange={this.onChange}
-                name="UNIQUE_ID_OF_DIV"
+                name="CodePane"
+                editorProps={{$blockScrolling: true}}
+                value={`function test(num) {
+    return num++;
+}
+test(8);`}
+            />
+            <AceEditor
+                mode="javascript"
+                theme="github"
+                onChange={this.onChange}
+                name="ResultPane"
                 editorProps={{$blockScrolling: true}}
             />
+            <button onClick={this.onClick}>Run</button>
             </div>
         )
     }
